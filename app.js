d54c460cfefa6a194704f1eef684481e266321d9
Axel <lezinaxel@gmail.com>
2016-10-24 15:10:15 +0200

    recuperation des nom

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-(function(){
+$(document).ready(function(){
+
+	"use strict"
 	$('#showData').on('click', function(){
 		$.ajax({
 			url : 'http://192.168.1.21:3000/places',
 			type : 'GET',
 			dataType : 'HTML',
 			success : function(resultat){
-				$('#data').html(resultat);
+				$.get("http://192.168.1.21:3000/places", function(response){
+				for (var i = 0; i < response.places.length; i++) {
+					response.places[i]
+					$("#data").append("<li>"+response.places[i].nom+"</li>")
+				}
+				console.log(resultat);
+				 // $('#data').html(resultat);
+
+				})
+				// var meteo = 'http://api.openweathermap.org/data/2.5/forecast/city?id=KEY HERE';
+
 			}
 		})
-	})
+
+
+	});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})();
+});
