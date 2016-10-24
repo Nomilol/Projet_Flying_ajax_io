0c275ee75422d03c9ae53fa53f5d86db54bd0a57
Axel <lezinaxel@gmail.com>
2016-10-24 16:51:48 +0200

    Début de l'affichage de la météo

--- app.js
+++ app.js
@@ -14,18 +14,20 @@ $(document).ready(function(){
 						response.places[i]
 						console.log(response.places[i]);
 						$("#data").append("<li>"+response.places[i].nom+"</li>")
-						
+
 					}
 
 					console.log(resultat);
 					 // $('#data').html(resultat);
 
 
-				})
+					})
 
 
-				},
-				// var meteo = 'http://api.openweathermap.org/data/2.5/forecast/city?id=KEY HERE';
+			},
+
+
+
 
 			
 		})
@@ -33,7 +35,25 @@ $(document).ready(function(){
 
 	});
 
+				// var meteo = 'http://api.openweathermap.org/data/2.5/forecast/city?id=KEY HERE';
+
+
+				$('#showMeteo').on('click', function(){
+					$.ajax({
+						url :"openweathermap.org/find?q=toulouse",
+						type : 'GET',
+						dataType : 'HTML',
+
+						success : function(resultat){
+				// on recupère les données de l'url
+				$.get("openweathermap.org/find?q=toulouse", function(response)
 
 
 
-});
+
+
+			});
+
+
+			});
+				});
