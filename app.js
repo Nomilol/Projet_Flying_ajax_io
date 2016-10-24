c58d6e2f9826d2c59fd710d9e5249b2f17eb3547
Axel <lezinaxel@gmail.com>
2016-10-24 14:07:56 +0200

    Récupération du tableau

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('hello');
+(function(){
+	$('#showData').on('click', function(){
+		$.ajax({
+			url : 'http://192.168.1.21:3000/places',
+			type : 'GET',
+			dataType : 'HTML',
+			success : function(resultat){
+				$('#data').html(resultat);
+			}
+		})
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
