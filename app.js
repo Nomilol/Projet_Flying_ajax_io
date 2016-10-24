a2442a18c7b72eb713afb5af08e5f78ba4933b0f
Axel <lezinaxel@gmail.com>
2016-10-24 15:50:53 +0200

    indentation et ajout commentaires

--- app.js
+++ app.js
@@ -7,18 +7,27 @@ $(document).ready(function(){
 			type : 'GET',
 			dataType : 'HTML',
 			success : function(resultat){
+				// on recupère les données de l'url
 				$.get("http://192.168.1.21:3000/places", function(response){
-				for (var i = 0; i < response.places.length; i++) {
-					response.places[i]
-					$("#data").append("<li>"+response.places[i].nom+"</li>")
-				}
-				console.log(resultat);
-				 // $('#data').html(resultat);
+					// on parcourt le tableau
+					for (var i = 0; i < response.places.length; i++) {
+						response.places[i]
+						console.log(response.places[i]);
+						$("#data").append("<li>"+response.places[i].nom+"</li>")
+						
+					}
+
+					console.log(resultat);
+					 // $('#data').html(resultat);
+
 
 				})
+
+
+				},
 				// var meteo = 'http://api.openweathermap.org/data/2.5/forecast/city?id=KEY HERE';
 
-			}
+			
 		})
 
 
